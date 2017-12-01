740236d594868fd365e6a2b56e0a226e3cb2565b
Abhinav Sharma <abhinav.ih@gmail.com>
2017-12-01 17:13:57 +0530

    Util created for EasyGrant

--- easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
+++ easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
@@ -2,43 +2,70 @@ package com.abhinav.easygrant
 
 import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Build
-import android.support.v4.app.ActivityCompat
+import android.content.Intent
 
 /**
  * Created by abhinav.sharma on 29/11/17.
  */
-class EasyGrantUtil {
+public class EasyGrantUtil(builder: Builder) : GrantCallbacks{
+
+
+    override fun onPermissionRequired() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionDenied() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionDisabled() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPermissionGranted() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRationaleNeeded() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     companion object {
         private lateinit var appContext: Context
+        private lateinit var easyGrantActivity: EasyGrantActivity
 
         fun getInstance(context: Context) {
             appContext = context
         }
-    }
 
-    private fun shouldAskPermission() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+        fun onPermissionResult(grantedPermissions: Array<String>, deniedPermissions: Array<String>){
 
-    private fun shouldAskPermission(context: Context, permission: String): Boolean {
-        if (shouldAskPermission()) {
-            return ActivityCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED
-        }
-        return false
-    }
-
-    public fun seekPermission(context: Context, permission: String, listener: GrantCallbacks){
-        when{
-            shouldAskPermission(context, permission) -> {
-                when{
-                    ActivityCompat.shouldShowRequestPermissionRationale(context as Activity, permission)
-                            -> listener.onRationaleNeeded()
-                    else -> listener.onPermissionRequired()
-                }
-            }
         }
     }
 
+    init {
+        val intent = Intent(builder.activity, EasyGrantActivity::class.java)
+        intent.putExtra("permissions", builder.permissions[0])
+        builder.activity.startActivity(intent)
+    }
+
+    class Builder {
+
+        lateinit var activity: Activity
+        lateinit var permissions: Array<String>
+
+        fun withActivity(activity: Activity): Builder {
+            this.activity = activity
+            return this
+        }
+
+        fun permissions(permissions: Array<String>): Builder {
+            this.permissions = permissions
+            return this
+        }
+
+        fun seek(): EasyGrantUtil = EasyGrantUtil(this)
+
+    }
 
 }
