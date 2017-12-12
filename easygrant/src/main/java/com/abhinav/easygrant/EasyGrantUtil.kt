a69c50d5dd4e2365c98f12f38574183921641646
abhinav272 <abhinav.ih@gmail.com>
2017-12-12 19:33:13 +0530

    multiple permissions support added

--- easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
+++ easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
@@ -3,6 +3,7 @@ package com.abhinav.easygrant
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import java.util.ArrayList
 
 /**
  * Created by abhinav.sharma on 29/11/17.
@@ -45,22 +46,28 @@ public class EasyGrantUtil(builder: Builder) : GrantCallbacks {
 
     init {
         val intent = Intent(builder.activity, EasyGrantActivity::class.java)
-        intent.putExtra("permissions", builder.permissions[0])
+        intent.putStringArrayListExtra("permissions", builder.permissions)
         builder.activity.startActivity(intent)
     }
 
     class Builder {
 
-        lateinit var activity: Activity
-        lateinit var permissions: Array<String>
+        internal lateinit var activity: Activity
+        internal lateinit var permissions: ArrayList<String>
+        internal lateinit var rationalMessages: ArrayList<String>
 
         fun withActivity(activity: Activity): Builder {
             this.activity = activity
             return this
         }
 
-        fun permissions(permissions: Array<String>): Builder {
-            this.permissions = permissions
+        fun permissions(permissions: List<String>): Builder {
+            this.permissions = permissions as ArrayList<String>
+            return this
+        }
+
+        fun rationalMessageList(rationalMessages: List<String>): Builder {
+            this.rationalMessages = rationalMessages as ArrayList<String>
             return this
         }
 
