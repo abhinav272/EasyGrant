c6652f0f5dc69149494651874152468287dc4b4b
abhinav272 <abhinav.ih@gmail.com>
2017-12-16 18:07:15 +0530

    Structure changed for builder

--- easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
+++ easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
@@ -8,52 +8,38 @@ import java.util.ArrayList
 /**
  * Created by abhinav.sharma on 29/11/17.
  */
-public class EasyGrantUtil(builder: Builder) : GrantCallbacks {
-
-
-    override fun onPermissionRequired() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onPermissionDenied() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onPermissionDisabled() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onPermissionGranted() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onRationaleNeeded() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+public class EasyGrantUtil(builder: Builder) {
 
     companion object {
         private lateinit var appContext: Context
         private lateinit var easyGrantActivity: EasyGrantActivity
+        private lateinit var callback: GrantCallbacks
 
         fun getInstance(context: Context) {
             appContext = context
         }
 
         fun onPermissionResult(grantedPermissions: Array<String>, deniedPermissions: Array<String>) {
-
+            callback.onPermissionDenied(deniedPermissions)
+            callback.onPermissionGranted(grantedPermissions)
         }
     }
 
     init {
         val intent = Intent(builder.activity, EasyGrantActivity::class.java)
-        intent.putStringArrayListExtra("permissions", builder.permissions)
+        intent.putParcelableArrayListExtra("multiple_permission", builder.permissionsRequest)
+        intent.putExtra("single_permission", builder.permissionRequest)
+        callback = builder.callback
         builder.activity.startActivity(intent)
     }
 
     class Builder {
 
         internal lateinit var activity: Activity
-        internal lateinit var permissions: ArrayList<String>
+        internal lateinit var permissionRequest: PermissionRequest
+        internal lateinit var permissionsRequest: ArrayList<PermissionRequest>
+        internal lateinit var callback: GrantCallbacks
+
         internal lateinit var rationalMessages: ArrayList<String>
 
         fun withActivity(activity: Activity): Builder {
@@ -61,13 +47,18 @@ public class EasyGrantUtil(builder: Builder) : GrantCallbacks {
             return this
         }
 
-        fun permissions(permissions: List<String>): Builder {
-            this.permissions = permissions as ArrayList<String>
+        fun withPermission(permissionRequest: PermissionRequest): Builder {
+            this.permissionRequest = permissionRequest
             return this
         }
 
-        fun rationalMessageList(rationalMessages: List<String>): Builder {
-            this.rationalMessages = rationalMessages as ArrayList<String>
+        fun withPermissions(permissionsRequest: List<PermissionRequest>): Builder {
+            this.permissionsRequest = permissionsRequest as ArrayList<PermissionRequest>
+            return this
+        }
+
+        fun setCallback(callback: GrantCallbacks): Builder{
+            this.callback = callback
             return this
         }
 
