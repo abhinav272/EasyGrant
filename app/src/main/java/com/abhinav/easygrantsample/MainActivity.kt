22f2078cdd415e68d3e976eeb110b968f0c74a04
abhinav272 <abhinav.ih@gmail.com>
2017-12-18 16:09:15 +0530

    docs added

--- app/src/main/java/com/abhinav/easygrantsample/MainActivity.kt
+++ app/src/main/java/com/abhinav/easygrantsample/MainActivity.kt
@@ -29,11 +29,11 @@ class MainActivity : AppCompatActivity(), GrantCallbacks {
         grantedPermissions.forEach { Log.e("granted", it.permissionName) }
     }
 
-
-    val CAM_PERMISSION = 1
-    private lateinit var permissions: ArrayList<String>
-    private lateinit var rationalMessages: ArrayList<String>
     private lateinit var permissionsList: ArrayList<PermissionRequest>
+
+    /**
+     * Create Permission Request
+     * */
     private var cameraPermission = PermissionRequest(Manifest.permission.CAMERA,
             "I need camera permission to show you world", 1)
     private var locationPermission = PermissionRequest(Manifest.permission.ACCESS_FINE_LOCATION,
@@ -45,15 +45,6 @@ class MainActivity : AppCompatActivity(), GrantCallbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        permissions = ArrayList()
-        rationalMessages = ArrayList()
-        permissions.add(Manifest.permission.CAMERA)
-        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
-        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        permissions.add(Manifest.permission.READ_SMS)
-        permissions.add(Manifest.permission.BODY_SENSORS)
-        rationalMessages.add("Need Camera Permission for my use")
-        rationalMessages.add("Need Location group permissions for my use")
         setContentView(R.layout.activity_main)
 
         btn_cam_permission.setOnClickListener {
@@ -73,6 +64,9 @@ class MainActivity : AppCompatActivity(), GrantCallbacks {
         }
     }
 
+    /**
+     * Sample to use Single Permission
+     * */
     private fun askCamPermission() {
         EasyGrantUtil.Builder()
                 .withActivity(this)
@@ -97,6 +91,9 @@ class MainActivity : AppCompatActivity(), GrantCallbacks {
                 .seek()
     }
 
+    /**
+     * Sample to use Multiple Permission
+     * */
     private fun askPermissionAll() {
         permissionsList = ArrayList()
         permissionsList.add(cameraPermission)
@@ -108,31 +105,5 @@ class MainActivity : AppCompatActivity(), GrantCallbacks {
                 .withPermissions(permissionsList)
                 .setCallback(this)
                 .seek()
-    }
-
-    private fun askPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-                    Log.e("askPermission", "shouldShowRationale")
-                    AlertDialog.Builder(this)
-                            .setMessage("You need to give Cam Permission")
-                            .setPositiveButton("OK", { dialog, which ->
-                                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAM_PERMISSION)
-                            })
-                            .show()
-
-                } else
-                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAM_PERMISSION)
-            } else {
-                Toast.makeText(applicationContext, "Cam Granted", Toast.LENGTH_SHORT).show()
-            }
-
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.e("RequestResult", "reqCode -> ${requestCode} , ${permissions[0]}, grantResult -> ${grantResults[0]}")
     }
 }
