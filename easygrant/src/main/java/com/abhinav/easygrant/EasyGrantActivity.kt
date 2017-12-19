31fab00898085a04590ae277743c065c8eb872d3
abhinav272 <abhinav.ih@gmail.com>
2017-12-19 19:45:49 +0530

    class refactoring and made internal

--- easygrant/src/main/java/com/abhinav/easygrant/EasyGrantActivity.kt
+++ easygrant/src/main/java/com/abhinav/easygrant/EasyGrantActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.WindowManager
 import java.util.ArrayList
 
