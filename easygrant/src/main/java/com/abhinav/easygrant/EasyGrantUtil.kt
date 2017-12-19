990ade90273d11fb588beda487af39c400c3f1c7
abhinav272 <abhinav.ih@gmail.com>
2017-12-19 19:44:24 +0530

    new util created

--- easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
+++ easygrant/src/main/java/com/abhinav/easygrant/EasyGrantUtil.kt
@@ -1,86 +1,26 @@
 package com.abhinav.easygrant
 
-import android.app.Activity
-import android.content.Intent
-import com.abhinav.easygrant.exception.IllegalEasyGrantBuilderException
 import java.util.ArrayList
 
 /**
- * Created by abhinav.sharma on 29/11/17.
+ * Created by abhinav.sharma on 19/12/17.
  */
-public class EasyGrantUtil(builder: Builder) {
+internal class EasyGrantUtil {
 
     internal companion object {
+
         private lateinit var callback: GrantCallbacks
 
+        internal fun setCallback(callbacks: GrantCallbacks){
+            callback = callbacks
+        }
+
         internal fun onPermissionResult(grantedPermissions: ArrayList<PermissionRequest>,
-                               deniedPermissions: ArrayList<PermissionRequest>,
-                               disabledPermissions: ArrayList<PermissionRequest>) {
+                                        deniedPermissions: ArrayList<PermissionRequest>,
+                                        disabledPermissions: ArrayList<PermissionRequest>) {
             callback.onPermissionDenied(deniedPermissions)
             callback.onPermissionGranted(grantedPermissions)
             callback.onPermissionDisabled(disabledPermissions)
         }
     }
-
-    init {
-        val intent = Intent(builder.activity, EasyGrantActivity::class.java)
-        if (builder.permissionsRequest?.size == 1 && builder.permissionRequest == null)
-            intent.putExtra("single_permission", builder.permissionsRequest!![0])
-        if (builder.permissionRequest != null)
-            intent.putExtra("single_permission", builder.permissionRequest)
-        if (builder.permissionsRequest != null && builder.permissionsRequest!!.size > 1)
-            intent.putParcelableArrayListExtra("multiple_permission", builder.permissionsRequest)
-        callback = builder.callback!!
-        builder.activity?.startActivity(intent)
-    }
-
-    /**
-     * Builder for Using EasyGrant
-     * @param activity -- mandatory
-     * @param callback -- mandatory
-     * @throws IllegalEasyGrantBuilderException if activity or callbacks not set or permissions
-     * not added.
-     * */
-    class Builder {
-
-        internal var activity: Activity? = null
-        internal var permissionRequest: PermissionRequest? = null
-        internal var permissionsRequest: ArrayList<PermissionRequest>? = null
-        internal var callback: GrantCallbacks? = null
-
-        internal lateinit var rationalMessages: ArrayList<String>
-
-        fun withActivity(activity: Activity): Builder {
-            this.activity = activity
-            return this
-        }
-
-        fun withPermission(permissionRequest: PermissionRequest): Builder {
-            this.permissionRequest = permissionRequest
-            return this
-        }
-
-        fun withPermissions(permissionsRequest: List<PermissionRequest>): Builder {
-            this.permissionsRequest = permissionsRequest as ArrayList<PermissionRequest>
-            return this
-        }
-
-        fun setCallback(callback: GrantCallbacks): Builder {
-            this.callback = callback
-            return this
-        }
-
-        fun seek(): EasyGrantUtil {
-            if (activity == null)
-                throw IllegalEasyGrantBuilderException("Activity not set in builder")
-            else if (callback == null)
-                throw IllegalEasyGrantBuilderException("Callbacks not set in builder")
-            else if (permissionRequest == null && permissionsRequest == null)
-                throw IllegalEasyGrantBuilderException("No Permissions added in builder")
-
-            return EasyGrantUtil(this)
-        }
-
-    }
-
 }
